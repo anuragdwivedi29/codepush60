b653cc3137eb1135e995149866078a79231b771b
Anurag <anurag@Anurags-MacBook-Pro.local>
2019-08-19 18:13:18 +0530

    Button added

--- App.js
+++ App.js
@@ -6,6 +6,7 @@
  * @flow
  */
 
+import codePush from "react-native-code-push";
 import React, {Fragment} from 'react';
 import {
   SafeAreaView,
@@ -14,6 +15,7 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -24,7 +26,14 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+onButtonPress=()=> {
+  codePush.sync({
+    updateDialog: true,
+    installMode: codePush.InstallMode.IMMEDIATE
+  })};
 const App = () => {
+
+  
   return (
     <Fragment>
       <StatusBar barStyle="dark-content" />
@@ -45,6 +54,10 @@ const App = () => {
                 Edit <Text style={styles.highlight}>App.js</Text> 
               </Text>
             </View>
+
+<TouchableOpacity onPress={this.onButtonPress}>
+  <Text>Check for updates</Text>
+</TouchableOpacity>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>See Your Changes</Text>
               <Text style={styles.sectionDescription}>
